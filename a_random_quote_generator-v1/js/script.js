56f739eab18845bedb761933fd8e1fa0af6cd104
Ryan Collins <ryan.k.collins222@gmail.com>
2020-03-02 11:46:29 -0800

    Change back colors

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -19,7 +19,8 @@ let quotes = [
   {
     quote: "The Dude abides.",
     source: "The Dude",
-    citation: "The Big Lebowski"
+    citation: "The Big Lebowski",
+    tag: "movie quotes"
   },
   {
     quote: "Be yourself; everyone else is already taken.",
@@ -29,7 +30,8 @@ let quotes = [
     quote: "I’m going to make him an offer he can’t refuse",
     source: "Don Corleone",
     citation: "The Godfather",
-    year: 1972
+    year: 1972,
+    tag: "movie quotes"
   },
   {
     quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
@@ -37,21 +39,22 @@ let quotes = [
   },
 ];
 
-/***
- * `getRandomQuote` function
-***/
-
+// calculates random index num to return quote 
 function getRandomQuote() {
   let randNum = Math.floor(Math.random() * quotes.length);
   return quotes[randNum];
 }
 
-/***
- * `printQuote` function
-***/
+// random color
+function randColor() {
+  let color = Math.floor(Math.random() * 256);
+  return color;
+}
 
+// prints quote, source to page. citation and year if available
 function printQuote() {
   let randQuote = getRandomQuote();
+  let rgbColor = `rgb(${randColor()}, ${randColor()}, ${randColor()})`
   let quoteString = `
     <p class="quote"> ${randQuote.quote} </p>
     <p class="source"> ${randQuote.source}
@@ -62,9 +65,13 @@ function printQuote() {
   if (randQuote.year) {
     quoteString += `<span class="year"> ${randQuote.year}</span>`
   }
+  if (randQuote.tag) {
+    quoteString += `<span class="tag">, ${randQuote.tag}</span>`
+  }
   quoteString += `</p>`
   document.getElementById('quote-box').innerHTML = quoteString;
-}
+  document.body.style.backgroundColor = rgbColor;
+} 
 
 /***
  * click event listener for the print quote button
